dc332b97b609b3daf63a76e5fa178120fae0b548
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2022-05-27 00:37:49 +0700

    :tada: init project with terraform framework template

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/provider"
+)
+
+//go:generate terraform fmt -recursive ./examples/
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
+var (
+	version string = "dev"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/pepodev/ansible",
+		Debug:   debug,
+	}
+
+	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
